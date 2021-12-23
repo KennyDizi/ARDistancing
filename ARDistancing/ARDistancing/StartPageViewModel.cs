59419ef5c915488b139ac071f793122358991b45
KennyDizi <xamarindevelopervietnam@outlook.com>
2021-12-23 22:21:39 +0700

    apply sextant navigation

---
+++ ARDistancing/ARDistancing/StartPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+using ARDistancing.Views;
+using ReactiveUI;
+using System;
+using Sextant;
+
+namespace ARDistancing
+{
+    public class StartPageViewModel : BasePageViewModel
+    {
+        public StartPageViewModel()
+        {
+            GotoArPageCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                // todo ask camera permission
+                // todo navigate to AR Page, integration with Setant
+                _viewStackService?.PushPage<MainPageViewModel>(animate: true).Subscribe();
+            });
+        }
+
+        public ICommand GotoArPageCommand { get; }
+    }
+}
--- ARDistancing/ARDistancing/Views/BasePageViewModel.cs
+++ ARDistancing/ARDistancing/Views/BasePageViewModel.cs
@@ -1,8 +1,22 @@
-﻿using ReactiveUI;
+﻿using System;
+using ReactiveUI;
+using Sextant;
+using Splat;
 
 namespace ARDistancing.Views
 {
-    public class BasePageViewModel : ReactiveObject
+    public abstract class BasePageViewModel : ReactiveObject, IViewModel
     {
+        protected IParameterViewStackService _viewStackService;
+
+        protected BasePageViewModel()
+        {
+            _viewStackService = Locator
+                .Current
+                .GetService<IParameterViewStackService>();
+            Id = new Random().Next().ToString();
+        }
+
+        public string Id { get; }
     }
 }
